150cc3b04ef86d63bccdd168e2b242e178bb15ab
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2018-05-21 20:45:44 +0300

    Add Clipper modes
    Clipper library has three separate modules
    which need to be encapsulated somehow.

--- clipper.cpp
+++ clipper.cpp
@@ -1,12 +1,36 @@
 #include "clipper.h"
 
+#define PRECISION 10000.0
+
+Clipper::Clipper(): mode(MODE_CLIP) {}
+
+void Clipper::add_path(const Vector<Vector2> &points, cl::PathType polytype, bool is_open) {
+
+    // Scaling needs to be taken into account as well
+
+    // switch(mode) {
+
+    //     case MODE_CLIP:
+    //         cl.AddPath()
+    // }
+}
+
 void Clipper::_bind_methods() {
+
+    ClassDB::bind_method(D_METHOD("set_mode", "mode"), &Clipper::set_mode);
+    ClassDB::bind_method(D_METHOD("get_mode"), &Clipper::get_mode);
+
+    ClassDB::bind_method(D_METHOD("add_path", "points", "path_type", "is_open"), &Clipper::add_path);
 
     ClassDB::bind_method(D_METHOD("get_version"), &Clipper::get_version);
 
     using namespace clipperlib;
     // Use namespace declaration to avoid having
     // prepended namespace name in enum constants
+    BIND_ENUM_CONSTANT(MODE_CLIP);
+    BIND_ENUM_CONSTANT(MODE_OFFSET);
+    BIND_ENUM_CONSTANT(MODE_TRIANGULATE);
+
     BIND_ENUM_CONSTANT(ctNone);
     BIND_ENUM_CONSTANT(ctIntersection);
     BIND_ENUM_CONSTANT(ctUnion);
--- clipper.h
+++ clipper.h
@@ -9,10 +9,23 @@
 
 namespace cl = clipperlib;
 
+enum ClipMode {
+    MODE_CLIP,
+    MODE_OFFSET,
+    MODE_TRIANGULATE
+};
+
 class Clipper : public Reference {
     GDCLASS(Clipper, Reference);
 
 public:
+    Clipper();
+
+    void add_path(const Vector<Vector2> &points, cl::PathType polytype, bool is_open);
+
+    void set_mode(ClipMode p_mode) { mode = p_mode; }
+    ClipMode get_mode() const { return mode; }
+
 //------------------------------------------------------------------------------
 // Meta methods
 //------------------------------------------------------------------------------
@@ -20,10 +33,19 @@ public:
 
 protected:
     static void  _bind_methods();
+
+private:
+    ClipMode mode;
+
+    cl::Clipper cl;
+    cl::ClipperOffset co;
+    cl::ClipperTri ct;
 };
 
-VARIANT_ENUM_CAST(clipperlib::ClipType);
-VARIANT_ENUM_CAST(clipperlib::PathType);
-VARIANT_ENUM_CAST(clipperlib::FillRule);
+VARIANT_ENUM_CAST(ClipMode);
+
+VARIANT_ENUM_CAST(cl::ClipType);
+VARIANT_ENUM_CAST(cl::PathType);
+VARIANT_ENUM_CAST(cl::FillRule);
 
 #endif
