73c5d930231e33132291bcc1696e4bb3b1ee94f8
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2018-05-21 23:41:51 +0300

    Somewhat working implementation combining all three Clipper modules
    TODO: need to add ability to retrieve solution as PolyPath
    (reconstructs hierarchical order of paths)

--- clipper.cpp
+++ clipper.cpp
@@ -2,28 +2,151 @@
 
 #define PRECISION 10000.0
 
-Clipper::Clipper(): mode(MODE_CLIP) {}
+Clipper::Clipper(): mode(MODE_CLIP),
+                    open(false),
+                    fill_rule(cl::frEvenOdd),
+                    path_type(cl::ptSubject),
+                    join_type(cl::kSquare),
+                    end_type(cl::kPolygon),
+                    clip_type(cl::ctUnion),
+                    delta(0.0) {}
 
-void Clipper::add_path(const Vector<Vector2> &points, cl::PathType polytype, bool is_open) {
+void Clipper::add_points(const Vector<Vector2>& points) {
 
-    // Scaling needs to be taken into account as well
+    const cl::Path& path = _scale_up(points, PRECISION);
 
-    // switch(mode) {
+    switch(mode) {
 
-    //     case MODE_CLIP:
-    //         cl.AddPath()
-    // }
+        case MODE_CLIP: {
+            cl.AddPath(path, path_type, open);
+        } break;
+
+        case MODE_OFFSET: {
+            co.AddPath(path, join_type, end_type);
+        } break;
+
+        case MODE_TRIANGULATE: {
+            ct.AddPath(path, path_type, open);
+        } break;
+    }
+}
+
+void Clipper::execute() {
+
+    switch(mode) {
+
+        case MODE_CLIP: {
+            cl.Execute(clip_type, solution_closed, solution_open, fill_rule);
+        } break;
+
+        case MODE_OFFSET: {
+            co.Execute(solution_closed, delta * PRECISION);
+        } break;
+
+        case MODE_TRIANGULATE: {
+            ct.Execute(clip_type, solution_closed, fill_rule);
+        } break;
+    }
+}
+
+void Clipper::clear() {
+
+    cl.Clear();
+    co.Clear();
+    ct.Clear();
+}
+
+int Clipper::get_solution_count() const {
+
+    return solution_closed.size();
+}
+
+Vector<Vector2> Clipper::get_solution(int idx) {
+
+    ERR_EXPLAIN("Solution not found");
+    ERR_FAIL_INDEX_V(idx, solution_closed.size(), Vector<Vector2>());
+
+    Vector<Vector2> points = _scale_down(solution_closed[idx], PRECISION);
+
+    return points;
+}
+
+cl::Path Clipper::_scale_up(const Vector<Vector2>& points, real_t scale) {
+
+    cl::Path path;
+    path.resize(points.size());
+
+    for(int i = 0; i != points.size(); ++i) {
+        path[i] = cl::Point64(
+            points[i].x * scale,
+            points[i].y * scale
+        );
+    }
+    return path;
+}
+
+Vector<Vector2> Clipper::_scale_down(const cl::Path& path, real_t scale) {
+
+    Vector<Vector2> points;
+    points.resize(path.size());
+
+    for(int i = 0; i != path.size(); ++i) {
+        points[i] = Point2(
+            static_cast<real_t>(path[i].x) / scale,
+            static_cast<real_t>(path[i].y) / scale
+        );
+    }
+    return points;
 }
 
 void Clipper::_bind_methods() {
 
+//------------------------------------------------------------------------------
+// Clipping methods
+//------------------------------------------------------------------------------
     ClassDB::bind_method(D_METHOD("set_mode", "mode"), &Clipper::set_mode);
     ClassDB::bind_method(D_METHOD("get_mode"), &Clipper::get_mode);
 
-    ClassDB::bind_method(D_METHOD("add_path", "points", "path_type", "is_open"), &Clipper::add_path);
+    ClassDB::bind_method(D_METHOD("add_points", "points"), &Clipper::add_points);
+    ClassDB::bind_method(D_METHOD("execute"), &Clipper::execute);
 
+    ClassDB::bind_method(D_METHOD("get_solution_count"), &Clipper::get_solution_count);
+    ClassDB::bind_method(D_METHOD("get_solution", "index"), &Clipper::get_solution);
+
+    ClassDB::bind_method(D_METHOD("clear"), &Clipper::clear);
+
+//------------------------------------------------------------------------------
+// Configuration methods
+//------------------------------------------------------------------------------
+    ClassDB::bind_method(D_METHOD("set_open", "is_open"), &Clipper::set_open);
+    ClassDB::bind_method(D_METHOD("is_open"), &Clipper::is_open);
+
+    ClassDB::bind_method(D_METHOD("set_path_type", "path_type"), &Clipper::set_path_type);
+    ClassDB::bind_method(D_METHOD("get_path_type"), &Clipper::get_path_type);
+
+    ClassDB::bind_method(D_METHOD("set_clip_type", "clip_type"), &Clipper::set_clip_type);
+    ClassDB::bind_method(D_METHOD("get_clip_type"), &Clipper::get_clip_type);
+
+    ClassDB::bind_method(D_METHOD("set_join_type", "join_type"), &Clipper::set_join_type);
+    ClassDB::bind_method(D_METHOD("get_join_type"), &Clipper::get_join_type);
+
+    ClassDB::bind_method(D_METHOD("set_end_type", "end_type"), &Clipper::set_end_type);
+    ClassDB::bind_method(D_METHOD("get_end_type"), &Clipper::get_clip_type);
+
+    ClassDB::bind_method(D_METHOD("set_fill_rule", "fill_rule"), &Clipper::set_fill_rule);
+    ClassDB::bind_method(D_METHOD("get_fill_rule"), &Clipper::get_fill_rule);
+
+    ClassDB::bind_method(D_METHOD("set_delta", "delta"), &Clipper::set_delta);
+    ClassDB::bind_method(D_METHOD("get_delta"), &Clipper::get_delta);
+
+//------------------------------------------------------------------------------
+// Meta methods
+//------------------------------------------------------------------------------
     ClassDB::bind_method(D_METHOD("get_version"), &Clipper::get_version);
 
+//------------------------------------------------------------------------------
+// Enums
+//------------------------------------------------------------------------------
     using namespace clipperlib;
     // Use namespace declaration to avoid having
     // prepended namespace name in enum constants
@@ -44,4 +167,14 @@ void Clipper::_bind_methods() {
     BIND_ENUM_CONSTANT(frNonZero);
     BIND_ENUM_CONSTANT(frPositive);
     BIND_ENUM_CONSTANT(frNegative);
+
+    BIND_ENUM_CONSTANT(kSquare);
+    BIND_ENUM_CONSTANT(kRound);
+    BIND_ENUM_CONSTANT(kMiter);
+
+    BIND_ENUM_CONSTANT(kPolygon);
+    BIND_ENUM_CONSTANT(kOpenJoined);
+    BIND_ENUM_CONSTANT(kOpenButt);
+    BIND_ENUM_CONSTANT(kOpenSquare);
+    BIND_ENUM_CONSTANT(kOpenRound);
 }
--- clipper.h
+++ clipper.h
@@ -21,10 +21,46 @@ class Clipper : public Reference {
 public:
     Clipper();
 
-    void add_path(const Vector<Vector2> &points, cl::PathType polytype, bool is_open);
-
+//------------------------------------------------------------------------------
+// Clipping methods
+//------------------------------------------------------------------------------
     void set_mode(ClipMode p_mode) { mode = p_mode; }
     ClipMode get_mode() const { return mode; }
+
+    void add_points(const Vector<Vector2>& points);
+    void execute();
+
+    int get_solution_count() const;
+    Vector<Vector2> get_solution(int idx);
+
+    void clear();
+
+//------------------------------------------------------------------------------
+// Configuration methods
+//------------------------------------------------------------------------------
+    // Path and execute configuration, define these before adding new paths
+    // Each path added will have the same configuration as the previous one
+
+    void set_open(bool p_is_open) { open = p_is_open; }
+    bool is_open() const { return open; }
+
+    void set_path_type(cl::PathType p_path_type) { path_type = p_path_type; }
+    cl::PathType get_path_type() const { return path_type; }
+
+    void set_clip_type(cl::ClipType p_clip_type) { clip_type = p_clip_type; }
+    cl::ClipType get_clip_type() const { return clip_type; }
+
+    void set_join_type(cl::JoinType p_join_type) { join_type = p_join_type; }
+    cl::JoinType get_join_type() const { return join_type; }
+
+    void set_end_type(cl::EndType p_end_type) { end_type = p_end_type; }
+    cl::EndType get_end_type() const { return end_type; }
+
+    void set_fill_rule(cl::FillRule p_fill_rule) { fill_rule = p_fill_rule; }
+    cl::FillRule get_fill_rule() const { return fill_rule; }
+
+    void set_delta(real_t p_delta) { delta = p_delta; }
+    real_t get_delta() const { return delta; }
 
 //------------------------------------------------------------------------------
 // Meta methods
@@ -35,7 +71,21 @@ protected:
     static void  _bind_methods();
 
 private:
+    cl::Path _scale_up(const Vector<Vector2>& points, real_t scale);
+    Vector<Vector2> _scale_down(const cl::Path& path, real_t scale);
+
+    bool open;
+    cl::FillRule fill_rule;
+    cl::PathType path_type;
+    cl::JoinType join_type;
+    cl::ClipType clip_type;
+    cl::EndType end_type;
+    real_t delta;
+
     ClipMode mode;
+
+    cl::Paths solution_closed;
+    cl::Paths solution_open;
 
     cl::Clipper cl;
     cl::ClipperOffset co;
@@ -47,5 +97,8 @@ VARIANT_ENUM_CAST(ClipMode);
 VARIANT_ENUM_CAST(cl::ClipType);
 VARIANT_ENUM_CAST(cl::PathType);
 VARIANT_ENUM_CAST(cl::FillRule);
+
+VARIANT_ENUM_CAST(cl::JoinType);
+VARIANT_ENUM_CAST(cl::EndType);
 
 #endif
