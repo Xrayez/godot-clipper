c2e889535984a74ce31e0e8619b0ffb160ef3bb2
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-04-08 13:50:11 +0300

    Add open paths offsetting in demo
    3.2.dev.Xrayez.5095c28

--- examples/projects/open_paths/clipper.gd
+++ examples/projects/open_paths/clipper.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var line = [Vector2(0, 100), Vector2(200, 100)]
+const DRAW_OFFSET = Vector2(50, 50)
+
+var line = [Vector2(0, 100), Vector2(200, 100), Vector2(200, 200)]
 var rect = [Vector2(50, 50), Vector2(150, 50), Vector2(150, 150), Vector2(50, 150)]
 
 var clipper = Clipper.new()
@@ -13,15 +15,30 @@ func _ready():
 func test_open_paths():
 	clipper.clear()
 
-	clipper.set_mode(Clipper.MODE_CLIP)
+	match clipper.mode:
 
-	clipper.path_type = Clipper.ptSubject
-	clipper.open = true # curve
-	clipper.add_points(line)
+		Clipper.MODE_CLIP:
 
-	clipper.path_type = Clipper.ptClip
-	clipper.open = false # polygon
-	clipper.add_points(rect)
+			clipper.path_type = Clipper.ptSubject
+			clipper.open = true # curve
+			clipper.add_points(line)
+
+			clipper.path_type = Clipper.ptClip
+			clipper.open = false # polygon
+			clipper.add_points(rect)
+
+		Clipper.MODE_OFFSET:
+
+			clipper.path_type = Clipper.ptSubject
+			clipper.open = true # curve
+
+			# Offset amount
+			clipper.delta = 20.0
+
+			# Creates square "passages" from open path (line):
+			clipper.end_type = Clipper.kOpenSquare
+
+			clipper.add_points(line)
 
 	clipper.execute()
 
@@ -30,12 +47,31 @@ func test_open_paths():
 
 func _draw():
 
-	draw_multiline(line, Color.magenta)
-	draw_colored_polygon(rect, Color.magenta)
+	print(true)
 
-	for idx in clipper.get_solution_count(Clipper.TYPE_OPEN):
-		var line = clipper.get_solution(idx, Clipper.TYPE_OPEN)
-		draw_multiline(line, Color.green)
+	draw_set_transform(DRAW_OFFSET, 0, Vector2.ONE)
+
+	match clipper.mode:
+
+		Clipper.MODE_CLIP:
+
+			draw_polyline(line, Color.red)
+			draw_colored_polygon(rect, Color.red)
+
+			for idx in clipper.get_solution_count(Clipper.TYPE_OPEN):
+				var line = clipper.get_solution(idx, Clipper.TYPE_OPEN)
+				draw_polyline(line, Color.green)
+
+		Clipper.MODE_OFFSET:
+
+			# Line offseting results in polygon
+
+			draw_polyline(line, Color.red)
+
+			for idx in clipper.get_solution_count(Clipper.TYPE_CLOSED):
+				var polygon = clipper.get_solution(idx, Clipper.TYPE_CLOSED)
+				draw_polyline(polygon, Color.green)
+				draw_line(polygon[0], polygon[-1], Color.green) # last
 
 
 func _on_operation_selected(ID):
@@ -47,6 +83,18 @@ func _on_operation_selected(ID):
 		2:
 			clipper.clip_type = Clipper.ctIntersection
 		3:
-			clipper.clip_type = Clipper.ctXor # same as union (?)
+			clipper.clip_type = Clipper.ctXor # same as union for lines (?)
+
+	test_open_paths()
+
+
+func _on_mode_selected(ID):
+	var is_reusing_solution = false
+
+	match ID:
+		0:
+			clipper.set_mode(Clipper.MODE_CLIP, is_reusing_solution)
+		1:
+			clipper.set_mode(Clipper.MODE_OFFSET, is_reusing_solution)
 
 	test_open_paths()
